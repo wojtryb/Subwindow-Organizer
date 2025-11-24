c0db56940a201c1cf5c4641a6d2255da15b57612
Wojciech Trybus <wojtryb@gmail.com>
2025-11-24 20:48:46 +0100

    Resize background windows on opening and closing views

--- subwindow_organizer/logic/mdi_area_logic.py
+++ subwindow_organizer/logic/mdi_area_logic.py
@@ -9,11 +9,30 @@ class MdiAreaLogic(MdiAreaFilter.Logic):
     def __init__(self, resizer: Resizer) -> None:
         self._resizer = resizer
         self._subwindow_filter = SubwindowFilter(SubwindowLogic(resizer))
+        self._background_windows = set()
 
     def on_subwindow_open(self, subwindow: QMdiSubWindow):
-        print(f"View Opened: {subwindow}")
         subwindow.installEventFilter(self._subwindow_filter)
 
+        amount = len(self._resizer.mdi_area.subWindowList())
+
+        if amount == 1:
+            self._resizer.left = subwindow
+            self._resizer.right = None
+            self._resizer.resize_background_subwindows()
+        elif amount == 2:
+            self._resizer.right = subwindow
+            self._resizer.resize_background_subwindows()
+        else:
+            self._resizer.resize_to_suggested_size(subwindow)
+
     def on_subwindow_close(self, subwindow: QMdiSubWindow):
-        print(f"View Closed: {subwindow}")
         subwindow.removeEventFilter(self._subwindow_filter)
+
+        if subwindow == self._resizer.left:
+            self._resizer.left = self._resizer.right
+            self._resizer.right = None
+            self._resizer.resize_background_subwindows()
+        elif subwindow == self._resizer.right:
+            self._resizer.right = None
+            self._resizer.resize_background_subwindows()
--- subwindow_organizer/logic/resizer.py
+++ subwindow_organizer/logic/resizer.py
@@ -1,10 +1,13 @@
 from PyQt5.QtWidgets import QMdiArea, QMdiSubWindow
+from PyQt5.QtCore import QSize
 
 
 class Resizer:
 
     def __init__(self, mdi_area: QMdiArea):
         self.mdi_area = mdi_area
+        self.left: QMdiSubWindow | None = None
+        self.right: QMdiSubWindow | None = None
 
     # snapping floaters to border of the canvas
     def snap_to_border(self, subwindow: QMdiSubWindow):
@@ -23,3 +26,30 @@ class Resizer:
             y = self.mdi_area.height() - subwindow.height()
 
         subwindow.move(x, y)
+
+    def resize_to_suggested_size(self, subwindow: QMdiSubWindow):
+        SUGGESTED_SIZE = QSize(400, 500)
+        subwindow.resize(SUGGESTED_SIZE)
+
+    # def swap_subwindows(self, a: QMdiSubWindow, b: QMdiSubWindow):
+    #     a_pos = a.pos()
+    #     b_pos = b.pos()
+    #     a_size = a.size()
+    #     b_size = b.size()
+
+    #     a.resize(b_size)
+    #     a.move(b_pos)
+    #     b.resize(a_size)
+    #     b.move(a_pos)
+
+    def resize_background_subwindows(self):
+        if self.left is not None and self.right is None:
+            self.left.resize(self.mdi_area.size())
+            self.left.move(0, 0)
+        if self.left is not None and self.right is not None:
+            width, height = self.mdi_area.width(), self.mdi_area.height()
+
+            self.left.resize(round(width * 0.5), height)
+            self.left.move(0, 0)
+            self.right.resize(round(width * 0.5), height)
+            self.right.move(width, 0)
